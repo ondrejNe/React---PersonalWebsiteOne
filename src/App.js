1c258c671185fe9b260c56f15143f1a34712299a
necasond <necasond@fel.cvut.cz>
2022-06-24 21:15:39 +0200

    Added: Basic website structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Footer } from './components/footer/Footer'
+import { Header } from './components/header/Header'
+import { Home } from './components/home/Home'
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+        <Home />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
